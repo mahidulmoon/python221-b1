e370561ebce0f4f3545e010d1ecf6fc222ca68aa
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-03-27 22:35:14 +0600

    register module working

--- finalProject/user/views.py
+++ finalProject/user/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def loginPage(request):
@@ -7,4 +8,20 @@ def loginPage(request):
 
 
 def registerPage(request):
-    return render(request,'html/register.html')
+    if request.method == "GET":
+        return render(request,'html/register.html')
+    elif request.method == "POST":
+        # print(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+
+        try:
+            User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+            messages.success(request,"User Creation successful")
+            return redirect("loginpage")
+        except Exception as e:
+            messages.error(request,"can not create user"+str(e))
+            return redirect("registerpage")
