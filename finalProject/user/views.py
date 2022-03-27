927415e43675c68da559ca948faf37741167c0e2
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-03-27 23:00:01 +0600

    login module working

--- finalProject/user/views.py
+++ finalProject/user/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,auth
 from django.contrib import messages
 # Create your views here.
 
 def loginPage(request):
-    return render(request,'html/login.html')
+    if request.method == "GET":
+        return render(request,'html/login.html')
+    elif request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,"Login successful")
+            return redirect('allblog')
+        else:
+            messages.error(request,"Invalid username and password")
+            return redirect('loginpage')
+
+
 
 
 def registerPage(request):
