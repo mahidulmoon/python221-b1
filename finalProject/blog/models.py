dbabac0086420ae18a8560900d5d59f94498f483
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-03-29 22:52:45 +0600

    todo model add

--- finalProject/blog/models.py
+++ finalProject/blog/models.py
@@ -1,3 +1,10 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100,blank=False,null=True)
+    description = models.TextField(blank=False,null=True)
+    img_file = models.ImageField(upload_to='todo/',default="")
