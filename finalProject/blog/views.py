c0ccccb60b870f05fb23236344223db8b48eec9a
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-03-29 23:08:59 +0600

    form insert successful with message

--- finalProject/blog/views.py
+++ finalProject/blog/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from email import message
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
+from .models import Todo
+from django.contrib import messages
 
 @login_required(login_url='/')
 def blogPage(request):
-    return render(request,'index.html')
+    if request.method == "GET":
+        return render(request,'index.html')
+    elif request.method == "POST":
+        title = request.POST["todotitle"]
+        description = request.POST["tododescription"]
+
+        try:
+            Todo.objects.create(title=title,description=description)
+            messages.success(request,"ToDo has been successfully created.")
+        except Exception as e:
+            messages.error(request,"Can not able to create todo "+str(e))
+        
+        return redirect('allblog')
