9ecd8ad53518339775a687801a9c40e8c728d1f5
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-03-29 22:49:06 +0600

    input form layout added

--- finalProject/blog/views.py
+++ finalProject/blog/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/')
 def blogPage(request):
-    return render(request,'blogIndex.html')
+    return render(request,'index.html')
